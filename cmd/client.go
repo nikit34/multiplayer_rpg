c16c1a3ca0eb4d4a0d5a9eb12cd823b57d3c3702
Никита Пермяков <permikov134@mail.ru>
2022-03-16 06:01:56 +0000

    Add client server iteraction

--- cmd/client.go
+++ cmd/client.go
@@ -26,4 +26,50 @@ func main() {
 	}
 
 	client := proto.NewGameClient(conn)
+	stream, err := client.Stream(context.Background())
+	if err != nil {
+		log.Fatalf("open stream error %v", err)
+	}
+
+	req := proto.Request{
+		Action: &proto.Request_connect{
+			Connect: &proto.Connect{
+				Player: playerName,
+			},
+		},
+	}
+
+	stream.Send(&req)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				log.Fatalf("EOF")
+				return
+			}
+			if err != nil {
+				log.Fatalf("can not receive %v", err)
+			}
+
+			init := resp.GetInitialize()
+			if init != nil {
+				currentPlayer := backend.Player{
+					Position: backend.Coordinate{
+						X: int(init.Position.X),
+						Y: int(init.Position.Y),
+					},
+					Name: playerName,
+					Direction: backend.DirectionStop,
+					Icon: 'P',
+				}
+				game.Mux.Lock()
+				game.Players[playerName] = &currentPlayer
+				game.Mux.Unlock()
+				view.CurrentPlayer = &currentPlayer
+			}
+		}
+	}()
+
+	view.Start()
 }
