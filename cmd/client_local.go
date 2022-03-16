a8a5edc2eafa36dcd90e15b8f64cf2c99cfdc576
Никита Пермяков <permikov134@mail.ru>
2022-03-16 05:44:28 +0300

    Change name file for local implementation

---
+++ cmd/client_local.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/nikit34/multiplayer_rpg_go/pkg/backend"
+	"github.com/nikit34/multiplayer_rpg_go/pkg/frontend"
+)
+
+
+func main() {
+	currentPlayer := backend.Player{
+		Position: backend.Coordinate{X: -1, Y: -5},
+		Name: "Alice",
+		Icon: 'A',
+		Direction: backend.DirectionStop,
+	}
+
+	game := backend.NewGame()
+	game.Players = append(game.Players, &currentPlayer)
+	game.CurrentPlayer = &currentPlayer
+
+	app := frontend.NewView(&game)
+
+	game.Start()
+	app.Start()
+}
