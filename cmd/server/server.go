09a2da4b0c936a23bda2365a761a979e0630f9b3
Nikita Permyakov <n.permyakov@aprotech.ru>
2022-04-07 10:01:27 +0300

    Allow server to run on arbitrary port

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"flag"
 	"log"
 	"net"
 
@@ -12,7 +14,10 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":8888")
+	port := *flag.Int("port", 8888, "Port to listen on")
+	log.Printf("listening on port %d", port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
